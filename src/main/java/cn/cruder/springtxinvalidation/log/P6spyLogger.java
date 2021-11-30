e6c3669dddd206789f31bfc2b0872df8d2ae7407
DouShaoxun <dousx_cn@163.com>
2021-11-30 22:52:50 +0800

    log

---
+++ src/main/java/cn/cruder/springtxinvalidation/log/P6spyLogger.java
@@ -0,0 +1,17 @@
+package cn.cruder.springtxinvalidation.log;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: cruder
+ * @Date: 2021/11/30/22:50
+ */
+@Slf4j
+public class P6spyLogger extends com.p6spy.engine.spy.appender.StdoutLogger {
+
+    @Override
+    public void logText(String text) {
+        //  SQL 日志
+        log.debug(text);
+    }
+}

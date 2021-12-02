f6bea49d6987a873bd6253b6ad00629f9e231ee7
DouShaoxun <dousx_cn@163.com>
2021-12-02 21:40:29 +0800

    默认Transactional加在非public方法上不生效

--- src/main/java/cn/cruder/springtxinvalidation/Application.java
+++ src/main/java/cn/cruder/springtxinvalidation/Application.java
@@ -2,6 +2,9 @@ package cn.cruder.springtxinvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
 
 /**
  * @author dousx
@@ -11,6 +14,21 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    /**
+     * 默认Transactional加在非public方法上不生效
+     * <br/>
+     * 设置{@link AnnotationTransactionAttributeSource#publicMethodsOnly}为false
+     * <br/>
+     * 则非public方法也可以生效
+     *
+     * @return {@link AnnotationTransactionAttributeSource}
+     */
+    @Bean
+    public TransactionAttributeSource transactionAttributeSource() {
+        return new AnnotationTransactionAttributeSource(false);
     }
 
 }
